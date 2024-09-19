b009388995f333ee7d0faa7f64435281077e3060
Ignas Deltuva <ignasdelt@gmail.com>
2024-09-20 01:33:38 +0300

    Remove unnecessary import

--- src/main/java/com/api/invoicer/component/CommandLineAppStartupRunner.java
+++ src/main/java/com/api/invoicer/component/CommandLineAppStartupRunner.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Slf4j
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
