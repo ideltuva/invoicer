a2a8a2af5dbf242e5a981fc85406fbe2fdeb5a34
Ignas Deltuva <ignasdelt@gmail.com>
2024-09-20 01:30:25 +0300

    Run service on startup

---
+++ src/main/java/com/api/invoicer/component/CommandLineAppStartupRunner.java
@@ -0,0 +1,24 @@
+package com.api.invoicer.component;
+
+import com.api.invoicer.facade.FileHandlerFacade;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CommandLineAppStartupRunner implements CommandLineRunner {
+
+    final FileHandlerFacade fileHandlerFacade;
+
+    public CommandLineAppStartupRunner(FileHandlerFacade fileHandlerFacade) {
+        this.fileHandlerFacade = fileHandlerFacade;
+    }
+
+    @Override
+    public void run(String... args) {
+        fileHandlerFacade.handleFiles();
+    }
+}
